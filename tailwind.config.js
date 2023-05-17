baade21265426b33332c0e5f072567544b729016
Quentin <kent.26780@gmail.com>
2023-05-17 13:57:44 +0200

    Version 1 of onlineCV

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: { 
+    colors:{
+     'darkblue': '#5C6F7E',
+     'lightblue': '#6096B4',
+     'lighterblue':'#93BFCF',
+     'cream': '#EEE9DA',
+     'gray': '#787878',
+     'black': '#070707',
+     'lightblack': '#2D3945'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
